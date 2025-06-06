e4b78e560b869028cb9bf5d103acbfcd54c38c5b
Dolphinh16 <miguelhmercedes@hotmail.com>
2025-06-06 21:12:08 +0000

    ApistarwarMMH

--- src/app.py
+++ src/app.py
@@ -8,7 +8,7 @@ from flask_swagger import swagger
 from flask_cors import CORS
 from utils import APIException, generate_sitemap
 from admin import setup_admin
-from models import db, User
+from models import db, User,Planeta, Personaje,Favorito
 #from models import Person
 
 app = Flask(__name__)
@@ -44,6 +44,84 @@ def handle_hello():
     }
 
     return jsonify(response_body), 200
+
+@app.route('/people', methods=['GET'])
+def handle_people():
+
+    people = Personaje.query.all()
+    people_serialize = [person.serialize() for person in people]
+    return jsonify(people_serialize), 200
+
+
+@app.route('/people/<int:id>', methods=['GET'])
+def get_one_person(id):
+    
+    gente = Personaje.query.get(id)
+    if gente is None:
+        return jsonify({"error": "Personaje no encontrado"}), 404
+    return jsonify(gente.serialize()), 200
+
+@app.route('/planet', methods=['GET'])
+def handle_planets():
+
+    planets = Planeta.query.all()
+    planets_serialize = [planeta.serialize() for planeta in planets]
+    return jsonify(planets_serialize), 200
+
+#Ruta planeta por id
+@app.route('/planet/<int:id>', methods=['GET'])
+def get_one_planet(id):
+
+    planeta = Planeta.query.get(id)
+    if planeta is None:
+        return jsonify({"error": "planeta no encontrado"}), 404
+    return jsonify(planeta.serialize()), 200
+
+#Ruta que muestre todos los usuarios
+
+@app.route('/user', methods=['GET'])
+def handle_users():
+
+    usuarios = User.query.all()
+    usuarios_serialize = [usuario.serialize() for usuario in usuarios]
+    return jsonify(usuarios_serialize), 200
+
+#Ruta que muestro un usuario por id
+
+@app.route('/user/<int:id>', methods=['GET'])
+def get_one_usuario(id):
+
+    users = User.query.get(id)
+    if users is None:
+        return jsonify({"error": "Usuario no encontrado"}), 404
+    return jsonify(users.serialize()), 200
+
+# Ruta para agregar favorito planetas
+@app.route('/favoritos/planeta/<int:id>', methods=['POST'])
+def add_datos(id):
+
+    
+    add_favorito = Favorito(
+        user_id=1,
+        planeta_id =id
+    )
+    db.session.add(add_favorito)
+    db.session.commit()
+    return jsonify({"msg":"Planeta favorito agregado"}), 201
+
+    
+
+    
+
+
+
+
+
+# Ruta para agregar favorito people
+
+
+
+
 
 # this only runs if `$ python src/app.py` is executed
 if __name__ == '__main__':
--- src/models.py
+++ src/models.py
@@ -1,19 +1,75 @@
 from flask_sqlalchemy import SQLAlchemy
-from sqlalchemy import String, Boolean
-from sqlalchemy.orm import Mapped, mapped_column
+from sqlalchemy import String, Boolean, ForeignKey
+from sqlalchemy.orm import Mapped, mapped_column, relationship
 
 db = SQLAlchemy()
 
 class User(db.Model):
     id: Mapped[int] = mapped_column(primary_key=True)
     email: Mapped[str] = mapped_column(String(120), unique=True, nullable=False)
-    password: Mapped[str] = mapped_column(nullable=False)
-    is_active: Mapped[bool] = mapped_column(Boolean(), nullable=False)
+    firstname: Mapped[str] = mapped_column(String(20), nullable=False)
+    lastname: Mapped[str] = mapped_column(String(20), nullable=False)
+    username: Mapped[str] = mapped_column(String(30), nullable=False)
 
 
     def serialize(self):
         return {
             "id": self.id,
             "email": self.email,
-            # do not serialize the password, its a security breach
+            "firstname": self.firstname,
+            "lastname": self.lastname,
+            "username": self.username,
+
+            
+        }
+    
+class Personaje (db.Model):
+    id: Mapped[int] = mapped_column(primary_key=True)
+    name: Mapped[str] = mapped_column(String(50), nullable=False)
+    descripcion: Mapped[str] = mapped_column(String(200), nullable=False)
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "name": self.name,
+            "descripcion": self.descripcion,
+
+        }
+
+
+
+class Planeta (db.Model):
+    id: Mapped[int] = mapped_column(primary_key=True)
+    name: Mapped[str] = mapped_column(String(50), nullable=False)
+    descripcion: Mapped[str] = mapped_column(String(200), nullable=False)
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "name": self.name,
+            "descripcion": self.descripcion,
+
+        }
+
+
+
+
+class Favorito (db.Model):
+    id: Mapped[int] = mapped_column(primary_key=True)
+    user_id: Mapped[int]= mapped_column(ForeignKey("user.id"))
+    user=relationship("User" , backref="favoritos")
+    personaje_id: Mapped[int]= mapped_column(ForeignKey("personaje.id"),nullable=True)
+    personaje= relationship("Personaje" , backref="favoritos")
+    planeta_id: Mapped[int]= mapped_column(ForeignKey("planeta.id"),nullable=True)
+    planeta = relationship("Planeta" , backref="favoritos")
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "user_id": self.user_id,
+            "personaje_id": self.personaje_id,
+            "planeta_id": self.planeta_id,
+
+
+            
         }
